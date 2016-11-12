e566802da46e0bad6c9918c662fe347cc13b8e56
JOSE CAJIDE <jose.ramon.cajide@gmail.com>
2016-11-12 23:18:25 +0100

    fix

--- test.php
+++ test.php
@@ -1,14 +1,15 @@
 <?php
 require_once '../vendor/autoload.php';
 
-
+define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
 
 use Google\Spreadsheet\ServiceRequestFactory;
 use Google\Spreadsheet\DefaultServiceRequest;
 
 function getGoogleTokenFromKeyFile() {
     $client = new Google_Client();
-    $client->setAuthConfig( __DIR__ . 'client_secret.json' );
+   # $client->setAuthConfig( __DIR__ . 'client_secret.json' );
+      $client->setAuthConfig(CLIENT_SECRET_PATH);
     $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
     if ($client->isAccessTokenExpired()) {
         $client->refreshTokenWithAssertion();
