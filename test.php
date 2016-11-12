c646227ef25a7533b80aac24bc6147fdf143c4a0
JOSE CAJIDE <jose.ramon.cajide@gmail.com>
2016-11-12 23:09:05 +0100

    fix

--- test.php
+++ test.php
@@ -1,13 +1,28 @@
 <?php
 require_once '../vendor/autoload.php';
 
-use Google\Spreadsheet\DefaultServiceRequest;
-use Google\Spreadsheet\ServiceRequestFactory;
 
-$serviceRequest = new DefaultServiceRequest($accessToken);
+
+use Google\Spreadsheet\ServiceRequestFactory;
+use Google\Spreadsheet\DefaultServiceRequest;
+
+function getGoogleTokenFromKeyFile() {
+    $client = new Google_Client();
+    $client->setAuthConfig( __DIR__ . 'client_secret_532495736607-29u7acn3al4qasrmv68dedd8tgiiioar.apps.googleusercontent.com.json' );
+    $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+    if ($client->isAccessTokenExpired()) {
+        $client->refreshTokenWithAssertion();
+    }
+
+    return $client->getAccessToken();
+}
+
+$tokenArray = getGoogleTokenFromKeyFile();
+
+$serviceRequest = new DefaultServiceRequest($tokenArray['access_token']);
 ServiceRequestFactory::setInstance($serviceRequest);
 
 $spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
 $spreadsheetFeed = $spreadsheetService->getSpreadsheetFeed();
-
-print $spreadsheetFeed
+$spreadsheet = $spreadsheetFeed->getByTitle('Title');
+?>
