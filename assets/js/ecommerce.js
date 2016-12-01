1bbab6ea7d50ec212fafe3b2a3a7b3c2cdc113c8
JOSE CAJIDE <jose.ramon.cajide@gmail.com>
2016-12-01 10:37:20 +0100

    import

---
+++ assets/js/ecommerce.js
@@ -0,0 +1,28 @@
+(function(){
+	var products = [];
+	var a = document.querySelectorAll('[data-product-id]');
+	for(var i = 0; i < a.length; i++) {
+			productName = a[i].getAttribute('data-product-name') ? a[i].getAttribute('data-product-name'): '(not set)',
+			productId = a[i].getAttribute('data-product-id') ? a[i].getAttribute('data-product-id'): '(not set)',
+			productList = a[i].getAttribute('data-product-list') ? a[i].getAttribute('data-product-list') : 'Productos recomendados',
+			productPosition = i;
+		
+			products.push({
+				'name': productName,       
+				'id': productId,
+				'list': productList,
+				'position': productPosition
+			})			
+	}		
+	  var ecommerceData = {
+	  	'event': 'evento_ecommerce',
+		'ecommerce' : {
+		  currencyCode : 'EUR',
+		  'impressions': products
+		}
+	  };
+	  
+	  dataLayer.push(ecommerceData);
+  	
+  	//return ecommerceData;
+})();
