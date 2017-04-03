df54baa69090f8648f79d25318c6da6443d2142f
JOSE CAJIDE <jose.ramon.cajide@gmail.com>
2017-04-03 14:51:03 +0200

    update

--- register.php
+++ register.php
@@ -281,8 +281,10 @@ include 'header.inc.php';
         var inputPassword = String($('#password').val());
         
         console.log("-> Clave:" + inputPassword);
+        
+        var clientIdSplit = ga.getAll()[0].get('clientId').split('.');
 
-        var clientId = String(ga.getAll()[0].get('clientId'));
+        var clientId = String(clientIdSplit[0]);
         
         console.log("-> clientId:" + clientId);        
         
