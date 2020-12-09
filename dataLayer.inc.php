df37697d21c7e0f90527ebfd3533b2786380c2f4
Jose Ramón Cajide <joseramoncajide@users.noreply.github.com>
2020-12-09 22:51:29 +0100

    test

--- dataLayer.inc.php
+++ dataLayer.inc.php
@@ -28,6 +28,52 @@ if(strpos($_SERVER['REQUEST_URI'], '/form.php') !== false || strpos($_SERVER['RE
 </script>
 
 
+<?php
+
+if(strpos($_SERVER['REQUEST_URI'], '/order_received.php') !== false) { 
+?>
+<script>
+
+dataLayer.push({
+  'ecommerce': {
+    'purchase': {
+      'actionField': {
+        'id': 'T12345',                         // Transaction ID. Required for purchases and refunds.
+        'affiliation': 'Online Store',
+        'revenue': '35.43',                     // Total transaction value (incl. tax and shipping)
+        'tax':'4.90',
+        'shipping': '5.99',
+        'coupon': 'SUMMER_SALE'
+      },
+      'products': [{                            // List of productFieldObjects.
+        'name': 'Triblend Android T-Shirt',     // Name or ID is required.
+        'id': '12345',
+        'price': '15.25',
+        'brand': 'Google',
+        'category': 'Apparel',
+        'variant': 'Gray',
+        'quantity': 1,
+        'coupon': ''                            // Optional fields may be omitted or set to empty string.
+       },
+       {
+        'name': 'Donut Friday Scented T-Shirt',
+        'id': '67890',
+        'price': '33.75',
+        'brand': 'Google',
+        'category': 'Apparel',
+        'variant': 'Black',
+        'quantity': 1
+       }]
+    }
+  }
+});
+</script>
+
+<?php
+} 
+?>
+
+
 <style>
 #barraaceptacion {
     display:none;
