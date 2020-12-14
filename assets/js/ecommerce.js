027c0cc392e9ed6d63143e59b001f30d6ed4e484
Jose Ramón Cajide <joseramoncajide@users.noreply.github.com>
2020-12-14 17:07:10 +0100

    eec js

--- assets/js/ecommerce.js
+++ assets/js/ecommerce.js
@@ -5,12 +5,14 @@
 			productName = a[i].getAttribute('data-product-name') ? a[i].getAttribute('data-product-name'): '(not set)',
 			productId = a[i].getAttribute('data-product-id') ? a[i].getAttribute('data-product-id'): '(not set)',
 			productList = a[i].getAttribute('data-product-list') ? a[i].getAttribute('data-product-list') : 'Productos recomendados',
+			productPrice = a[i].getAttribute('data-product-price') ? a[i].getAttribute('data-product-price') : '',
 			productPosition = i;
 		
 			products.push({
 				'name': productName,       
 				'id': productId,
 				'list': productList,
+				'price': productPrice,
 				'position': productPosition
 			})			
 	}		
